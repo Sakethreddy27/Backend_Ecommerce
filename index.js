856a49042b8a3915ed67ce009f57d86a0a66b193
Sakethreddy27 <sakethreddy466@gmail.com>
2025-02-20 20:39:17 -0600

    updated index

--- index.js
+++ index.js
@@ -8,19 +8,14 @@ const firmRoutes = require('./routes/firmroutes');
 const productRoutes = require('./routes/productRoutes');
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 dotEnv.config();
 
 // Debugging to check MONGO_URI
 console.log("MongoDB URI:", process.env.MONGO_URI);
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 60000, // 60 seconds timeout for connection
-    socketTimeoutMS: 60000, // 60 seconds socket timeout
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("MongoDB connected successfully");
 })
@@ -48,6 +43,6 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-    console.log(`Server running successfully at http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server running successfully at http://localhost:${port}`); 
 });
